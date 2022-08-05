bae814a4b82afc3e53599827f57e40fcabfa88ae
bjlli <brendajacomelli@gmail.com>
2022-08-05 13:38:53 +0100

    creating a class in /sys/class

--- 7segment/7segment.c
+++ 7segment/7segment.c
@@ -4,30 +4,49 @@
 #include <linux/kernel.h>
 #include <linux/sysfs.h>
 #include <linux/kobject.h>
-
+#include <linux/fs.h>
+#include <linux/slab.h>
 
 /***************Global variables*****************/
 static struct class *device_class = NULL;
 volatile int value_display; 
 volatile int enable_dp;
-
+/***************Global Structs******************/
+static const struct file_operations fops = {
+	.owner = THIS_MODULE,
+};
 
 
 
 /***************Module Functions****************/
-static int __init segmentsDisplay_init(void)
+static int segmentsDisplay_init(void)
 {
+    int ret;
+    static struct lock_class_key key; 
+    printk(KERN_ALERT "7 segmente display module init!");
+
+    /*Creating a class in /sys/class*/    
+    device_class = (struct class *)kzalloc(sizeof(struct class),GFP_ATOMIC);;       if(!device_class){
+    	printk("Class allocation error!");
+    }
+    printk("Teste");
+    device_class->name = "segments_display";
+    device_class->owner = THIS_MODULE;
+    ret = __class_register(device_class,&key);
+
     
-    printk(KERN_ALERT "7 segment display module init!");
 
     return 0;
 
 }
 
-static void __exit segmentsDisplay_exit(void)
+static void segmentsDisplay_exit(void)
 {
     
+    class_unregister(device_class);
+    class_destroy(device_class);
     printk(KERN_ALERT "7 segment display module exit!");
+
 
 }
 
